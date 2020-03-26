8718778ce97f2b25177d538fd4d80eefbf82cd0d
Ben Jackson <benjamincjackson@gmail.com>
2020-03-26 17:35:15 +0000

    Update __init__.py

--- datafunk/subcommands/__init__.py
+++ datafunk/subcommands/__init__.py
@@ -1,3 +1,3 @@
-__all__ = ["remove_dat_junk", "repair_names","clean_names","remove_fasta","merge_fasta"]
+__all__ = ["remove_dat_junk", "repair_names","clean_names","remove_fasta","merge_fasta","get_ID_from_json_dict.py"]
 
 from datafunk.subcommands import *
