d43f10da8b702e998609a7773faac31af1ec7903
Ben Jackson <benjamincjackson@gmail.com>
2020-03-26 17:34:53 +0000

    Update __init__.py

--- datafunk/__init__.py
+++ datafunk/__init__.py
@@ -5,6 +5,6 @@ try:
 except:
     __version__ = "local"
 
-__all__ = ["remove_dat_junk","repair_names","clean_names","remove_fasta","merge_fasta"]
+__all__ = ["remove_dat_junk","repair_names","clean_names","remove_fasta","merge_fasta","get_ID_from_json_dict"]
 
 from datafunk import *
