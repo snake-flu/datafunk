5b3847308bbf25a1d38e75950273ca94ed322cb3
Rachel Colquhoun <rmnorris@well.ox.ac.uk>
2020-03-31 16:20:54 +0100

    fix typo and finish tidy up

--- tests/filter_low_coverage_test.py
+++ tests/filter_low_coverage_test.py
@@ -1,15 +1,14 @@
 import os
 import unittest
-import json
 import filecmp
 
-from datafunk.remove_low_coverage import *
+from datafunk.filter_low_coverage import *
 
 this_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-data_dir = os.path.join(this_dir, 'tests', 'data', 'remove_low_coverage')
+data_dir = os.path.join(this_dir, 'tests', 'data', 'filter_low_coverage')
 
-class TestRemoveLowCoverage(unittest.TestCase):
-    def threshold_70(self):
+class TestFilterLowCoverage(unittest.TestCase):
+    def test_threshold_70(self):
         infile = "%s/test.fasta" %data_dir
         outfile = "%s/tmp.threshold_70.fasta" %data_dir
         threshold = 70
@@ -18,7 +17,7 @@ class TestRemoveLowCoverage(unittest.TestCase):
         self.assertTrue(filecmp.cmp(outfile, expected, shallow=False))
         os.unlink(outfile)
 
-    def threshold_80(self):
+    def test_threshold_80(self):
         infile = "%s/test.fasta" %data_dir
         outfile = "%s/tmp.threshold_80.fasta" %data_dir
         threshold = 80
@@ -27,7 +26,7 @@ class TestRemoveLowCoverage(unittest.TestCase):
         self.assertTrue(filecmp.cmp(outfile, expected, shallow=False))
         os.unlink(outfile)
 
-    def threshold_90(self):
+    def test_threshold_90(self):
         infile = "%s/test.fasta" %data_dir
         outfile = "%s/tmp.threshold_90.fasta" %data_dir
         threshold = 90
@@ -36,7 +35,7 @@ class TestRemoveLowCoverage(unittest.TestCase):
         self.assertTrue(filecmp.cmp(outfile, expected, shallow=False))
         os.unlink(outfile)
 
-    def threshold_100(self):
+    def test_threshold_100(self):
         infile = "%s/test.fasta" %data_dir
         outfile = "%s/tmp.threshold_100.fasta" %data_dir
         threshold = 100
