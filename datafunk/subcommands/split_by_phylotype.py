c444c146b41378080b2db04e082ecd6277e26dc0
Xiaoyu Yu <57363085+xiaoyu518@users.noreply.github.com>
2020-04-06 16:49:31 +0100

    Delete split_by_phylotype.py

--- datafunk/subcommands/split_by_phylotype.py
+++
@@ -1,5 +0,0 @@
-from datafunk.split_by_phylotype import *
-
-def run(options):
-    phylo_dictionary = import_file(options.input_metafile,options.input_fasta)
-    split_phylotype(options.output_folder,phylo_dictionary,options.clade_threshold)
