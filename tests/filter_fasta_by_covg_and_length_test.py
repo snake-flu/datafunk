7f645e8a3184832338cf3bfa9f058b13857f3f17
Rachel Colquhoun <rmnorris@well.ox.ac.uk>
2020-04-07 09:29:12 +0100

    update length filtering to account for Ns at the start and end of sequences

--- tests/filter_fasta_by_covg_and_length_test.py
+++ tests/filter_fasta_by_covg_and_length_test.py
@@ -8,6 +8,30 @@ this_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 data_dir = os.path.join(this_dir, 'tests', 'data', 'filter_fasta_by_covg_and_length')
 
 class TestFilterFastaByCovgAndLength(unittest.TestCase):
+    def test_sequence_too_short_true(self):
+        sequence = "----AGCT---"
+        length_threshold = 5
+        result = sequence_too_short(sequence, length_threshold)
+        self.assertTrue(result)
+
+    def test_sequence_too_short_false(self):
+        sequence = "----AGCT---"
+        length_threshold = 4
+        result = sequence_too_short(sequence, length_threshold)
+        self.assertFalse(result)
+
+    def test_sequence_too_short_ns_true(self):
+        sequence = "NNNNAGCTNNNN"
+        length_threshold = 5
+        result = sequence_too_short(sequence, length_threshold)
+        self.assertTrue(result)
+
+    def test_sequence_too_short_ns_false(self):
+        sequence = "NNNNAGCTNNNN"
+        length_threshold = 4
+        result = sequence_too_short(sequence, length_threshold)
+        self.assertFalse(result)
+
     def test_threshold_70(self):
         infile = "%s/test.fasta" %data_dir
         outfile = "%s/tmp.threshold_70.fasta" %data_dir
