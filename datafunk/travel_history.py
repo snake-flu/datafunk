d8491ef4be4c4476dd5a76aeaa1df6621a9153fa
benjamincjackson <benjamincjackson@gmail.com>
2020-04-21 15:37:43 +0100

    update travel history

--- datafunk/travel_history.py
+++ datafunk/travel_history.py
@@ -13,21 +13,23 @@ countries_list = [x.name.lower() for x in pycountry.countries]
 
 subdivisions_dict = {x.name.lower(): x.country_code for x in pycountry.subdivisions}
 
-others = {'hubei': 'China',
-          'wuhan': 'China',
-          'korea': 'South Korea',
-          'us': 'USA',
-          'u.s.a': 'USA',
-          'usa': 'USA',
-          'u.k': 'UK',
-          'uk': 'UK',
-          'gran.canary': 'Spain',
-          'irn': 'Iran',
-          'ny': 'USA',
-          'la': 'USA',
-          'iran': 'Iran',
-          'fareo': 'Faroe Islands',
-          'czech': 'Czech Republic'}
+others = {'hubei': ('China', 'Hubei'),
+          'wuhan': ('China', 'Hubei'),
+          'korea': ('South_Korea', ''),
+          'us': ('USA', ''),
+          'u.s.a': ('USA', ''),
+          'usa': ('USA', ''),
+          'u.k': ('UK', ''),
+          'uk': ('UK', ''),
+          'gran.canary': ('Spain', 'Canary_Islands'),
+          'irn': ('Iran', ''),
+          'ny': ('USA', 'New_York_City'),
+          'la': ('USA', 'Los_Angeles'),
+          'iran': ('Iran', ''),
+          'fareo': ('Faroe_Islands', ''),
+          'czech': ('Czech_Republic', ''),
+          'finnland': ('Finland', ''),
+          'prague': ('Czech_Republic', 'Prague')}
 
 
 def get_travel_history(json_dict):
@@ -50,36 +52,46 @@ def get_travel_history(json_dict):
             continue
 
         if word.lower() in countries_list:
-            country.append(word)
-            continue
+            country.append((word.replace(' ', '_'), ''))
 
         if word.lower() in subdivisions_dict:
-            subdivision.append(word)
-            sd_country = pycountry.countries.get(alpha_2=subdivisions_dict[word.lower()]).name
-            if sd_country == 'Iran, Islamic Republic of':
-                country.append('Iran')
-            else:
-                country.append(sd_country)
-            continue
+            if word.lower() not in ['hubei', 'wuhan']:
+                subdivision.append(word)
+                sd_country = pycountry.countries.get(alpha_2=subdivisions_dict[word.lower()]).name
+                if sd_country == 'Iran, Islamic Republic of':
+                    country.append('Iran')
+                else:
+                    country.append((sd_country.replace(' ', '_'), word))
 
         if word.lower() in cities_dict:
-            city.append(word)
-            city_country = pycountry.countries.get(alpha_2=cities_dict[word.lower()]).name
-            country.append(city_country)
-            continue
+            if word.lower() not in ['hubei', 'wuhan', 'prague']:
+                city.append(word)
+                city_country = pycountry.countries.get(alpha_2=cities_dict[word.lower()]).name
+                country.append((city_country.replace(' ', '_'), word))
 
         if word.lower() in others:
             country.append(others[word.lower()])
 
-    everything_else = set(subdivision + city + other)
-    countries = set(country)
+    locales = set(country)
 
     # sometimes sampling country is represented in the text. Let's remove whereever
     # this is from the set, if it's in there
-    sampling_country = json_dict['edin_admin_0']
+    sampling_country = json_dict['edin_admin_0'].replace(' ', '_')
 
-    countries = countries - set([sampling_country])
+    def test_sampling_country(item):
+        return item[0] != sampling_country
 
-    json_dict['edin_travel'] = ':'.join(countries)
+    locales = set(filter(test_sampling_country, locales))
+
+    country_only = [x for x in locales if len(x[1]) == 0]
+    country_and_subdivision = [x for x in locales if len(x[1]) > 0]
+
+    for country in country_only:
+        if any([country[0] == x[0] for x in country_and_subdivision]):
+            locales.remove(country)
+
+    locales = [x[0] + '/' + x[1] if len(x[1]) > 0 else x[0] for x in locales]
+
+    json_dict['edin_travel'] = ';'.join(locales)
 
     return(json_dict)
