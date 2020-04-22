cb7ba19183d0ca29f23b6c4819d976b2f34c4cae
Ben Jackson <benjamincjackson@gmail.com>
2020-04-22 15:33:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="datafunk",
-    version="0.0.5",
+    version="0.0.6",
     packages=find_packages(),
     url="https://github.com/cov-ert/datafunk",
     license="MIT",
