ba416b44b7e0e7d0d2bcb27697546bd474d4bbaa
benjamincjackson <benjamincjackson@gmail.com>
2020-04-23 12:06:10 +0100

    new function

--- datafunk/subcommands/__init__.py
+++ datafunk/subcommands/__init__.py
@@ -1,6 +1,6 @@
 __all__ = ["repair_names","clean_names","remove_fasta","merge_fasta",
            "filter_fasta_by_covg_and_length", "process_gisaid_sequence_data", "sam_2_fasta",
            "phylotype_consensus", "gisaid_json_2_metadata", "set_uniform_header", "add_epi_week",
-           "process_gisaid_data", "pad_alignment"]
+           "process_gisaid_data", "pad_alignment", "exclude_uk_seqs"]
 
 from datafunk.subcommands import *
