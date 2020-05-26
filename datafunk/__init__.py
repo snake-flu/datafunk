7d395e571fcf864345ec0dc07fe3fd55bd87fc44
benjamincjackson <benjamincjackson@gmail.com>
2020-05-26 14:07:33 +0100

    add a function to anonymize microreact data

--- datafunk/__init__.py
+++ datafunk/__init__.py
@@ -9,6 +9,7 @@ __all__ = ["repair_names","clean_names","remove_fasta","merge_fasta",
            "filter_fasta_by_covg_and_length", "process_gisaid_sequence_data", "sam_2_fasta",
            "phylotype_consensus", "gisaid_json_2_metadata", "set_uniform_header", "add_epi_week",
            "process_gisaid_data", "pad_alignment", "exclude_uk_seqs", "get_CDS",
-           "distance_to_root", "mask", "curate_lineages", "snp_finder", "add_header_column"]
+           "distance_to_root", "mask", "curate_lineages", "snp_finder", "add_header_column",
+           "anonymize_microreact"]
 
 from datafunk import *
