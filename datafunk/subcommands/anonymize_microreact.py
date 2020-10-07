c6ab9c44e610504d2b821f054af1c672de2867c3
benjamincjackson <benjamincjackson@gmail.com>
2020-10-07 12:54:50 +0100

    remove

--- datafunk/subcommands/anonymize_microreact.py
+++
@@ -1,7 +0,0 @@
-from datafunk.anonymize_microreact import *
-
-def run(options):
-        anonymize_microreact(metadata_in = options.metadata_in,
-                             tree_in = options.tree_in,
-                             metadata_out = options.metadata_out,
-                             tree_out = options.tree_out)
