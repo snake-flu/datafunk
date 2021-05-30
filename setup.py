0ddc5422761bbf9da6da20d73a9f4685831b2db8
Rachel Colquhoun <rachel.colquhoun@ed.ac.uk>
2021-05-30 08:44:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="datafunk",
-    version="0.0.9",
+    version="0.1.0",
     packages=find_packages(),
     package_data={'datafunk':['datafunk/resources/*']},
     include_package_data=True,
